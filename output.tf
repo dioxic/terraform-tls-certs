99171021254992490db4260b192b25fc0898e761
Mark Baker-Munton <dioxic@gmail.com>
2021-04-21 10:20:26 +0100

    add sensitive flag to output variables

--- output.tf
+++ output.tf
@@ -1,13 +1,16 @@
 output "private_key_pem" {
   value = tls_private_key.main.private_key_pem
+  sensitive = true
 }
 
 output "cert_pem" {
   value = tls_locally_signed_cert.main.cert_pem
+  sensitive = true
 }
 
 output "combined_pem" {
   value = join("",[tls_locally_signed_cert.main.cert_pem, tls_private_key.main.private_key_pem])
+  sensitive = true
 }
 
 output "dns_names" {
